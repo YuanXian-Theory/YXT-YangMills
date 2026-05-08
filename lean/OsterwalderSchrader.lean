5a7b6df43e2d684e9b1e85d61138396bdde81518
Zhenyuan Acharya <yuanxian.philosophy@gmail.com>
2026-05-08 14:32:40 +0800

    Create OsterwalderSchrader.lean

---
+++ lean/OsterwalderSchrader.lean
@@ -0,0 +1,9 @@
+import Mathlib.Analysis.InnerProductSpace.Basic
+import "Quantization"
+
+theorem OS_reflection_positivity (M : Type _) [T64 M] : True := by
+  -- TCSC involution induces positive definite inner product
+  sorry
+
+theorem satisfies_OS_axioms (M : Type _) [T64 M] : True := by
+  sorry
